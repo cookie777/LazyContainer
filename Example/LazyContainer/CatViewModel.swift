d369197d9968e5c888991d40615d35dacf9039fb
cookie777 <@gmail.com>
2023-01-11 19:17:26 -0800

    feat(View): Add Cat view

---
+++ Example/LazyContainer/CatViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  CatViewModel.swift
+//  LazyContainer_Example
+//
+//  Created by Takayuki Yamaguchi on 2023-01-11.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+import LazyContainer
+
+class CatViewModel: ObservableObject {
+    
+    @Published var cats: [Cat] = []
+    private let container: LazyContainer
+    
+    internal init(container: LazyContainer) {
+        self.container = container
+    }
+    
+    func getCats() {
+        let catRepository: CatRepository = container.resolve()
+        Task {
+            let cats = await catRepository.getLatestCats()
+            await MainActor.run(body: {
+                self.cats = cats
+            })
+        }
+    }
+}
